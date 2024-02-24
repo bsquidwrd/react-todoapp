637e99a7480b724ff32ddc2d0a831ad22aaecfb4
bsquidwrd <git@bsquid.io>
2024-02-24 19:23:06 +0000

    Focus on text field after submit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,24 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./form.module.css";
 
 export default function Form({ todos, setTodos }) {
   // const [todo, setTodo] = useState("");
   const [todo, setTodo] = useState({ name: "", done: false });
+  const inputRef = useRef(null);
 
   function handleSubmit(event) {
     event.preventDefault();
     setTodos([...todos, todo]);
     setTodo({ name: "", done: false });
+    inputRef.current.focus();
   }
 
   return (
     <form className={styles.todoform} onSubmit={(e) => handleSubmit(e)}>
       <div className={styles.inputContainer}>
         <input
+          autoFocus
+          ref={inputRef}
           className={styles.modernInput}
           onChange={(e) => setTodo({ name: e.target.value, done: false })}
           placeholder="Enter todo item..."
