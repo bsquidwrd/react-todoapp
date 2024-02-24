228cd5edc4578fb46fa870aba4980f007a5a1c35
bsquidwrd <git@bsquid.io>
2024-02-24 04:00:02 +0000

    React For Beginners 30

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Todo from "./components/Todo";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Todo />
   </React.StrictMode>
 );
